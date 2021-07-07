e3f0a4c76be46fc27756794213ea01c22e34e52f
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-07-07 15:24:05 +0500

    admin.py ga tillar sozlandi

--- basic/admin.py
+++ basic/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from parler.admin import TranslatableAdmin
+from .models import *
 # Register your models here.
+
+admin.site.register(CustomUser)
+
+
+admin.site.register(Country)
+
+admin.site.register(Staff)
+
+admin.site.register(IdeaStartapper)
+
+admin.site.register(AllUsersIdea)
+
+admin.site.register(ApplicationStaff)
+
+@admin.register(SuccessProjects)
+class SuccessProjectsAdmin(TranslatableAdmin):
+    list_display = ('title', 'description',)
+
+
+# admin.site.register(SuccessProjects)
+admin.site.register(CommentOfPost)
+
+@admin.register(AboutUS)
+class AboutUSAdmin(TranslatableAdmin):
+    list_display = ('post_title', 'post_description',)
+
+admin.site.register(ContacktsProwork)
+admin.site.register(ProworkAdress)
