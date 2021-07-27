54afc621ffd13b04cba1725e516394baab1b20a6
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2021-07-28 00:00:01 +0500

    oddiy paginator qo`shildi

--- core/settings.py
+++ core/settings.py
@@ -58,8 +58,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 5
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
 
 MIDDLEWARE = [
